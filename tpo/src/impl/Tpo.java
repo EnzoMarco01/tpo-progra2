bf04af9977c9a300fc38a64842a5a7c5869d59f8
EnzoMarco01 <emarco@uade.edu.ar>
2024-06-13 17:51:10 -0300

    clas tpo

---
+++ tpo/src/impl/Tpo.java
@@ -0,0 +1,45 @@
+package impl;
+
+import interfaz.TpoTDA;
+
+public class Tpo implements TpoTDA {
+    public void listarProvincias() {
+
+    }
+
+    public void listarCiudades() {
+
+    }
+
+    public void cargarCiudades() {
+
+    }
+
+    public void eliminarCiudades() {
+
+    }
+
+    public void listarCiudadesVecinas() {
+
+    }
+
+    public void listarCiudadesPuente() {
+
+    }
+
+    public void listarCiudadesPredecesoras() {
+
+    }
+
+    public void listarCiudadesExtremo() {
+
+    }
+
+    public void listarCiudadesFuertementeConectadas() {
+
+    }
+
+    public void calcularCamino() {
+
+    }
+}
